d704a1dc6ae56ebc09d8b719848bafc115fccc7b
Adnaan Shirazi <a4adnaan@gmail.com>
2014-12-26 10:25:21 +0000

    Updates

--- MavenTest/src/main/java/com/adnaan/App.java
+++ MavenTest/src/main/java/com/adnaan/App.java
@@ -6,7 +6,7 @@ package com.adnaan;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!");  test checking
     }
 
 }
